3a32fd14f35709d6cd0b9a0d25dea2ef052dd7cf
JC Ketterer <jcketterer18@gmail.com>
2022-04-22 16:07:16 -0700

    created store, part of reducer, and helper file

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -7,3 +7,42 @@ const discountValues = {
   REMOVE20: 0.2,
   REMOVE30: 0.3,
 }
+
+const INIT_STATE = {
+  products: data.products,
+  itemsInCart: {},
+  totalCartValue: 0.0,
+  appliedDiscount: false,
+  discountTotal: 0,
+}
+
+const rootReducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case ADD_TO_CART: {
+      const cart = { ...state.itemsInCart }
+      cart[action.id] = (cart[action.id] || 0) + 1
+      return {
+        ...state,
+        itemsInCart: cart,
+        totalCartValue: calcTotal(state.products, cart, state.discountTotal),
+      }
+    }
+    case REMOVE_FROM_CART: {
+      const cart = { ...state.itemsInCart }
+      if (!cart[action.id]) return state
+      cart[action.id]--
+      if (cart[action.id] === 0) {
+        delete cart[action.id]
+      }
+      return {
+        ...state,
+        itemsInCart: cart,
+        totalCartValue: calcTotal(state.products, cart, state.discountTotal),
+      }
+    }
+    default:
+      return state
+  }
+}
+
+export default rootReducer
